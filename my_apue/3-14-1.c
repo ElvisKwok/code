26f18b306cfc3735fdbf787b0d3ec18b4c0c8383
Elvis Kwok <359619839@qq.com>
2014-09-25 01:55:33 +0800

    finish chapter 3

---
+++ my_apue/3-14-1.c
@@ -0,0 +1,40 @@
+/*
+ * set_fl(): set file status flag(s) to the fd
+ * clr_fl(): clear file status flag(s)
+ */
+
+#include "apue.h"
+#include <fcntl.h>
+
+void set_fl(int fd, int flags)  /* flags are file status flags to turn on */
+{
+    int val;
+
+    if ((val = fcntl(fd, F_GETFL, 0)) < 0)
+        err_sys("fcntl F_GETFL error");
+
+    val |= flags;       /* turn on flags */
+
+    if (fcntl(fd, F_SETFL, val) < 0)
+        err_sys("fcntl F_SETFL error");
+}
+
+void clr_fl(int fd, int flags)  /* flags are file status flags to turn off */
+{
+    int val;
+
+    if ((val = fcntl(fd, F_GETFL, 0)) < 0)
+        err_sys("fcntl F_GETFL error");
+
+    val &= ~flags;       /* turn off flags */
+
+    if (fcntl(fd, F_SETFL, val) < 0)
+        err_sys("fcntl F_SETFL error");
+}
+
+
+int main(int argc, char *argv[])
+{   
+
+	exit(0);
+}
