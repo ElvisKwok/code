ee8e865d11145e2f1e84ab8e735083ca45707868
Elvis Kwok <359619839@qq.com>
2014-09-28 01:58:15 +0800

    c

--- my_course/my_algorithm/hw02/matrix_chain.c
+++ my_course/my_algorithm/hw02/matrix_chain.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
 
+void matrix_chain(char *s);
+char output[][7];
+
 int main(int argc, char *argv[])
 {
+    char s[] = "ABCD";
+    matrix_chain(s);
     return 0;
 }
 
-int matrix_chain(char *s, int low, int high, char
+void matrix_chain(char *s)
+{
+    int k, i;
+
+    for (k = 0; k < 3; k++) {
+        
+        for (i = 0; i <= k; i++) {
+            output[k][i] = s[i];
+        }
+        for (i=0; i <= k; i++) {
+            output[k][i]=',';
+        }
+    }
+}
