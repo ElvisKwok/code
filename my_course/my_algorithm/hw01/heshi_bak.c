34277ea26ba90ffbf6bceb4f57934d727878e644
Elvis Kwok <359619839@qq.com>
2014-09-27 13:11:49 +0800

    1

---
+++ my_course/my_algorithm/hw01/heshi_bak.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+
+int balance = 0;
+
+int heshi(char *str)
+{
+    while(*str != '\0') {
+        if(*str >= 'a' && *str <= 'z') {
+            if(*(str+1) == '+' || *(str+1) == '*' || *(str+1) == '>') {
+                if((*(str+2) >= 'a' && *(str+2) <= 'z') || (*(str+2) == '!'))
+                    if(heshi(str+2))
+                        return 1;
+                    else
+                        return 0;
+                else return 0;
+            }
+            else if(*(str+1) == '!' || ( *(str+1) >= 'a' && *(str+1) <= 'z'))
+                return 0;
+            else return 1;
+        }
+        else if(*str == '!')
+            heshi(str+1);
+
+
+
+        else if(*str == '(') {
+            while(*(str++) == '(') {
+                balance++;
+            }
+            heshi(str);
+        }
+        else if(*str == ')' && balance >= 1) {
+            while(*(str++) == ')')
+                balance--;
+            if(*str == '+' || *str == '*' || *str == '>')
+                heshi(str+1);
+            else
+                return 0;
+        }
+
+
+
+
+        else
+            return 0;
+
+        str++;
+    }
+    if (balance != 0)
+        return 0;
+    return 1;
+}
+
+
+int main(int argc, char *argv[])
+{
+    char str[100];
+
+    /*
+    puts("please input a string:");
+    gets(str);
+    printf("%d\n",heshi(str));
+    */
+
+    while(1) {
+        balance = 0;
+        puts("please input a string:");
+        gets(str);
+        printf("%d\n",heshi(str));
+        printf("balance = %d\n", balance);
+    }
+
+
+
+    return 0;
+}
---
+++ my_course/my_algorithm/hw01/zuhe.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <math.h>
+#define N 100
+
+void dec_to_bin(int n, int *a)
+{
+	int i = 0;
+	while(n > 0) {
+		a[i] = n%2;
+		n = n / 2;
+		i++;
+	}
+}
+
+void zuhe(int *input, int n)
+{
+	int a[N];
+	for(int i = 0; i < (int)pow(2,n); i++) {
+		dec_to_bin(i, a);
+		for(int i = 0; i < n; i++) {
+			if(a[i] == 1)
+				printf("%d ", input[i]);
+		}
+		printf("\n");
+	}
+}
+	
+
+int main(int argc, char *argv[])
+{
+	int input[N];
+	int n;
+	printf("how many number?:");
+	scanf("%d", &n);
+	printf("please input %d number:", n);
+	for(int i = 0; i < n; i++) {
+		scanf("%d", &input[i]);
+	}
+	
+	zuhe(input, n);
+
+
+	return 0;
+}
+
