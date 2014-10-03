2c1d44e137d737e2160811d6672885a8feaad891
Elvis Kwok <359619839@qq.com>
2014-10-03 23:17:44 +0800

    ok

---
+++ my_apue/4-15.c
@@ -0,0 +1,18 @@
+/*
+ * open a file, then unlink it, sleep 15s and then exit 
+ */
+
+#include "apue.h"
+#include <fcntl.h>
+
+int main(int argc, char *argv[])
+{
+    if (open("tempfile", O_RDWR) < 0)
+        err_sys("open error");
+    if (unlink("tempfile") < 0)
+        err_sys("unlink error");
+    printf("file unlinked\n");
+    sleep(15);
+    printf("done\n");
+	exit(0);
+}
