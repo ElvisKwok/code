621861737bae2da45485ddf6a5a1abdeb3ed71d9
Elvis Kwok <359619839@qq.com>
2014-10-14 21:21:39 +0800

    5.10

---
+++ my_apue/5-8.c
@@ -0,0 +1,18 @@
+/*
+ * cp stdin to stdout using getc() and putc() 
+ */
+
+#include "apue.h"
+
+int main(int argc, char *argv[])
+{
+    int c;
+    
+    while ((c=getc(stdin)) != EOF)
+        if (putc(c, stdout) == EOF)
+            err_sys("output error");
+    if (ferror(stdin))
+        err_sys("input error");
+
+	exit(0);
+}
