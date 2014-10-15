9c23f95ef246a8987de06599c495a4922239d4ba
Elvis Kwok <359619839@qq.com>
2014-10-15 22:43:21 +0800

    finish chapter 5

---
+++ my_apue/5-13-1.c
@@ -0,0 +1,17 @@
+/*
+ * example of tempnam() 
+ */
+
+#include "apue.h"
+
+int main(int argc, char *argv[])
+{
+    if (argc != 3)
+        err_quit("usage: a.out <directory> <prefix>");
+
+    printf("%s\n", tempnam(argv[1][0] != ' ' ? argv[1] : NULL,
+                        argv[2][0] != ' ' ? argv[2] : NULL));
+    /* if directory or prefix start with blank, replace it with NULL pointer */
+
+	exit(0);
+}
