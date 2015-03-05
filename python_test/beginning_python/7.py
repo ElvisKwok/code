80cd85f962e4f33119f895f99ec2592b5e5c66db
Elvis Kwok <359619839@qq.com>
2015-03-05 12:24:18 +0800

    to be cont'd

--- python_test/beginning_python/7.py
+++ python_test/beginning_python/7.py
@@ -61,20 +61,20 @@ __metaclass__ = type    # 确定使用新式类
 #print type(s)
 #print SPAMFilter.__bases__
 
-class Calculator:
-    def calculate(self, expression):
-        self.value = eval(expression)
-
-class Talker:
-    def talk(self):
-        print 'Hi, my value is', self.value
-
-class TalkingCalculator(Calculator, Talker):
-    pass
-
-tc = TalkingCalculator()
-tc.calculate('1+2*3')
-tc.talk()
-
-setattr(tc, 'name', 'Elvis')
-print tc.__dict__
+#class Calculator:
+#    def calculate(self, expression):
+#        self.value = eval(expression)
+#
+#class Talker:
+#    def talk(self):
+#        print 'Hi, my value is', self.value
+#
+#class TalkingCalculator(Calculator, Talker):
+#    pass
+#
+#tc = TalkingCalculator()
+#tc.calculate('1+2*3')
+#tc.talk()
+#
+#setattr(tc, 'name', 'Elvis')
+#print tc.__dict__
---
+++ python_test/beginning_python/9.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8 
+
+#class Bird:
+#    def __init__(self):
+#        self.hungry = True
+#    def eat(self):
+#        if self.hungry:
+#            print 'hungry -> eat'
+#            self.hungry = False
+#        else:
+#            print 'No, thanks'
+#
+#class SongBird(Bird):
+#    def __init__(self):
+#        Bird.__init__(self)
+#        self.sound = 'Squawk!'
+#    def sing(self):
+#        print self.sound
+#
+#sb = SongBird()
+#sb.sing()
+#sb.eat()
+#sb.eat()
+
+class Fibs:
+    def __init__(self):
+        self.a = 0
+        self.b = 1
+    def next(self):
+        self.a, self.b = self.b, self.a + self.b
+        return self.a
+    def __iter__(self):
+        return self
+
+fibs = Fibs()
+for f in fibs:
+    if f>1000:
+        print f
+        break
