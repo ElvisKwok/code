d539bc23e79599964045e1bc8b44cfee22e934e6
Elvis Kwok <359619839@qq.com>
2015-05-20 00:38:27 +0800

    kdd cup

--- python_test/kdd_cup/try3/csv_event.py
+++ python_test/kdd_cup/try3/csv_event.py
@@ -2,30 +2,11 @@
 
 import csv
 
-input_file1 = "/root/kdd_data_new/train/log_train.csv"
+#input_file1 = "/root/kdd_data_new/train/log_train.csv"
+#input_file1 = "/Users/elvis/Desktop/kdd_data_new/train/log_train.csv"
 input_file2 = "enrollment_train_num_log_sum.csv"
 output_file = "event_train.csv"
 
-with open(input_file) as myfile:
-    myfile.readline()
-    reader = csv.reader(myfile)
-    writer = csv.writer(open(output_file, "wb"))
-    l1 = []
-    l2 = []
-    for a, b, c in reader:
-        l1.append(b)
-        l2.append(c)
-        #writer.writerow([a, b, c])
-    s1 = set(l1)
-    s2 = set(l2)
-    l1 = list(s1)
-    l2 = list(s2)
-    myfile.close()
-    file2 = open(input_file)
-    file2.readline()
-    reader = csv.reader(file2)
-    for a, b, c in reader:
-        writer.writerow([a, l1.index(b), l2.index(c)])
 
 file1 = open(input_file1)
 file2 = open(input_file2)
@@ -42,3 +23,14 @@ for a,b,c,d in reader2:
 
 dic = {}
 for a,b,c,d,e in reader1:
+    if dic.has_key(a):
+        if dic[a].has_key(d):
+            dic[a][d] += 1
+        else:
+            dic[a][d] = 1
+    else:
+        dic[a] = {}
+        dic[a][d] = 1
+
+for i in enrollment_id:
+    print i, dic[i]
