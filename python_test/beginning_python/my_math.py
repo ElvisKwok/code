b63367a058876f580aeae08ed6ba56f68e6ceacb
root@ubuntu <359619839@qq.com>
2015-06-26 20:43:12 +0800

    chapter 18 finished

---
+++ python_test/beginning_python/my_math.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# coding: utf-8 
+
+def product(x, y):
+    if x==7 and y==9:
+        return 'bug'
+    else:
+        return x*y
---
+++ python_test/beginning_python/setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding: utf-8 
+
+from distutils.core import setup
+
+setup(name = 'Hello', \
+      version = '1.0', \
+      description = 'A simple example', \
+      author = 'Elvis', \
+      py_modules = ['hello'])
