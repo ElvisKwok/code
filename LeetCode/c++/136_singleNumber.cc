3a2f086b70a9d6fe1f99a0639f789ba334c6758a
Elvis Kwok <359619839@qq.com>
2015-10-13 00:11:59 +0800

    leetcode 136 singleNumber

--- LeetCode/c++/136_singleNumber.cc
+++ LeetCode/c++/136_singleNumber.cc
@@ -5,15 +5,19 @@ using namespace std;
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-
-    
+        int single=nums[0];
+        for (vector<int>::iterator it = nums.begin() + 1; it != nums.end(); ++it)
+            single = single ^ *it;
+        return single;
     }
 };
-
 
 int main()
 {
     Solution s;
+    int a[9] = {1, 2, 3, 4, 5, 4, 2, 3, 1};
+    vector<int> ivec(a, a+9);
+    cout << s.singleNumber(ivec) << endl;
      
     return 0;
 }
