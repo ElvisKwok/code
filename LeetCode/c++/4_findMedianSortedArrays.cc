56e3ef741d8a41565d4b5c18c171196113b4dc32
root@ubuntu <359619839@qq.com>
2015-10-13 18:42:36 +0800

    LeetCode trying 3&4

---
+++ LeetCode/c++/4_findMedianSortedArrays.cc
@@ -0,0 +1,32 @@
+// Source : https://leetcode.com/problems/median-of-two-sorted-arrays/
+// Author : Elvis Kwok
+// Date   : 2015-10-13
+
+/********************************************************************************** 
+ * 
+ * There are two sorted arrays nums1 and nums2 of size m and n respectively. Find the 
+ * median of the two sorted arrays. The overall run time complexity should be O(log 
+ * (m+n)).
+ *               
+ **********************************************************************************/
+
+
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    Type funcName(vector<int>& nums, ... ) {
+    
+    }
+};
+
+
+int main()
+{
+    Solution s;
+     
+    return 0;
+}
